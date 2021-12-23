5d2e7b3dcbe7292f09183e8107f942403d0d3b24
venkat-rajgopal <venkatramani.r@gmail.com>
2021-12-23 11:16:28 +0100

    run everything in docker workingdir

--- create_tables.sql
+++ create_tables.sql
@@ -10,12 +10,12 @@ GRANT ALL PRIVILEGES ON test_sql.* TO 'vra'@'%';
 USE test_sql_database;
 
 CREATE TABLE auto_types (
-    i_id_car int,
-    s_model_code varchar(25),
-    s_color_code varchar(25),
-    s_extras varchar(100),
-    i_right_side int,
-    s_city_to_ship varchar(25)
+    i_id_car         int,
+    s_model_code     varchar(25),
+    s_color_code     varchar(25),
+    s_extras         varchar(100),
+    i_right_side     int,
+    s_city_to_ship   varchar(25)
 );
 
 INSERT INTO auto_types (i_id_car, s_model_code, s_color_code, s_extras, i_right_side, s_city_to_ship) VALUES (1, "GOLF2021", "BLUE7", "COND_AIR, GPS, MULTIMEDIA_V3", 0, "Barcelona");
--- run_mysql.sh
+++ run_mysql.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# cd /var/mysql_dock_test/
 # start service
 sudo service mysql start
-
 
 # run the sql script
 mysql -u root < create_tables.sql 
